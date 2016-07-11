c48e66d4d1153796bdf38f36afabf7addf6b5f44
mafuhua <862698384@qq.com>
2016-07-11 16:45:54 +0800

    7/11导入galleryfinal

--- app/src/main/java/com/yuen/yizu/ApplicationEx.java
+++ app/src/main/java/com/yuen/yizu/ApplicationEx.java
@@ -1,6 +1,15 @@
 package com.yuen.yizu;
 
 import android.app.Application;
+import android.graphics.Color;
+
+import com.yuen.yizu.galleryfinal.GlideImageLoader;
+import com.yuen.yizu.galleryfinal.GlidePauseOnScrollListener;
+
+import cn.finalteam.galleryfinal.CoreConfig;
+import cn.finalteam.galleryfinal.FunctionConfig;
+import cn.finalteam.galleryfinal.GalleryFinal;
+import cn.finalteam.galleryfinal.ThemeConfig;
 
 /**
  * Created by BaoHang on 15/3/23.
@@ -28,7 +37,40 @@ public class ApplicationEx extends Application {
         mApplication = this;
 
 
+        //建议在application中配置
+        //设置主题
+        // ThemeConfig theme = ThemeConfig.ORANGE;
+        ThemeConfig theme = new ThemeConfig.Builder()
+                .setTitleBarBgColor(Color.parseColor("#FF7478"))
+                .setTitleBarTextColor(Color.WHITE)
+                .setTitleBarIconColor(Color.WHITE)
+                .setFabNornalColor(Color.parseColor("#FF7478"))
+                .setFabPressedColor(Color.parseColor("#FF7478"))
+                .setCheckNornalColor(Color.WHITE)
+                .setCheckSelectedColor(Color.parseColor("#FF7478"))
+                .build();
+
+      /*  ThemeConfig theme = new ThemeConfig.Builder()
+                .build();*/
+        //配置功能
+        FunctionConfig functionConfig = new FunctionConfig.Builder()
+                .setEnableCamera(false)
+                .setEnableEdit(true)
+                .setEnableCrop(true)
+                .setEnableRotate(true)
+                .setCropSquare(true)
+                .setEnablePreview(true)
+                .build();
+        CoreConfig coreConfig = new CoreConfig.Builder(this, new GlideImageLoader(), theme)
+                .setFunctionConfig(functionConfig)
+                .setPauseOnScrollListener(new GlidePauseOnScrollListener(false, true))
+                .build();
+        GalleryFinal.init(coreConfig);
+        // sHA1(context);
+
     }
+
+
 
 
 }
